5b279961642effc2995a8b6f2e0db1751daef517
Marisilia Garcia <marisiliaemilia@gmail.com>
2023-04-12 15:46:46 -0500

    saves changes

---
+++ index.js
@@ -0,0 +1,43 @@
+function add() {
+    return "something";
+    
+}
+function add(a, b) {
+    return a + b;
+  }
+
+function subtract() {
+    return "something";
+}
+function subtract(a,b) {
+    return a - b;
+}
+
+function multiply() {
+    return "something";
+}
+function multiply(a,b) {
+    return a * b;
+}
+
+function divide() {
+    return "something";
+}
+function divide(a,b) {
+    return a / b;
+}
+
+function increment(n) {
+    return n + 1;
+}
+
+function decrement(n) {
+    return n - 1;
+}
+
+function makeInt(string) {
+    return parseInt(string, 10)
+}
+function preserveDecimal(string) {
+    return parseFloat(string)
+}
